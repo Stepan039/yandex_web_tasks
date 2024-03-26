98cc6fab7e15bb41948e20098f5fd52bf4c85062
Stepan039 <stefan039@bk.ru>
2024-03-26 20:20:25 +0200

    mars_image with bootstrap

--- main.py
+++ main.py
@@ -28,12 +28,27 @@ def image():
                     <html lang="en">
                       <head>
                         <meta charset="utf-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                        <link rel="stylesheet" 
+                        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                        crossorigin="anonymous">
+                        <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
                         <title>Привет, Марс!</title>
                       </head>
                       <body>
                         <h1>Жди нас, Марс!</h1>
                         <img src="{url_for('static', filename='img/mars.jpg')}" 
-           alt="здесь должна была быть картинка, но не нашлась">
+                            alt="здесь должна была быть картинка, но не нашлась">
+                        <div class="alert alert-primary" role="alert">
+                            Человечество вырастает из детства.
+                        </div>
+                        <div class="alert alert-success" role="alert">
+                            Человечеству дана одна планета.
+                        </div>
+                        <div class="alert alert-secondary" role="alert">
+                            И так далее...
+                        </div>
                       </body>
                     </html>"""
 
