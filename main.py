37c97f440e9631b8c3c142ad6b6fc2127ada9c6c
Stepan039 <stefan039@bk.ru>
2024-03-27 09:34:28 +0200

    mars planet choice

--- main.py
+++ main.py
@@ -140,5 +140,34 @@ def form():
         return "Форма отправлена"
 
 
+@app.route("/choice/<string:planet>")
+def choice(planet):
+    return f'''<!doctype html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                       <link rel="stylesheet"
+                       href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                       integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                       crossorigin="anonymous">
+                        <title>Варианты выбора</title>
+                      </head>
+                      <body>
+                        <h1>Моё предложение: {planet}</h1>
+                        <h2>Планета близка к Земле</h2>
+                        <div class="alert alert-primary" role="alert">
+                          На ней много ресурсов
+                        </div>
+                        <div class="alert alert-success" role="alert">
+                          Она красивая
+                        </div>
+                        <div class="alert alert-danger" role="alert">
+                          И так далее...
+                        </div>
+                      </body>
+                    </html>'''
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
